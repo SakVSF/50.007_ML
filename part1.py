649e454e11d98870a4027e4c7597839f32a25ad0
Saakshi Vinod Saraf <sarafsaakshivinod@gmail.com>
2023-03-27 22:05:03 +0800

    Create EvalScript

--- part1.py
+++ part1.py
@@ -1,6 +1,6 @@
 #part1
 import numpy as np
- 
+import random
 
 #define datapaths 
 en_train_path = "EN/train"
@@ -59,9 +59,9 @@ def read_dev_out_data(path):
                 results.append((token, labels[label]))
     return results
 
-def calculate_number_of_labels(input_data):
+def calculate_number_of_labels(data):
     label_counts = {}
-    for elem in input_data:
+    for elem in data:
         label = elem[1]
         if label in label_counts:
             label_counts[label] += 1
@@ -70,38 +70,92 @@ def calculate_number_of_labels(input_data):
     return label_counts
 
 
-def get_all_unique_tokens(input_data):
+def get_all_tokens(data):
     unique_tokens = []
-    for item in input_data:
+    for item in data:
         if item[0] not in unique_tokens:
             unique_tokens.append(item[0])
     return unique_tokens
 
 
-def calculate_emission_parameters(data, all_unique_tokens, k=1.0):
+def calculate_emission_parameters(data, all_tokens, k=1.0):
 
-    # Final index is for #UNK# tokens
-   
+    # the extra +1 column is for #UNK# tokens
+    emission_counts = np.zeros((N, len(all_tokens) + 1), dtype=np.longdouble)   #creates an NP array where rows - labels, columns - unique tokens
+    emission_parameters = np.zeros((N, len(all_tokens) + 1), dtype=np.longdouble)
 
-    emission_counts = np.zeros((N, len(all_unique_tokens) + 1), dtype=np.longdouble)   #creates an NP array where rows - labels, columns - unique tokens
-    emission_parameters = np.zeros((N, len(all_unique_tokens) + 1), dtype=np.longdouble)
+    # calculate count(label -> token) 
+    for token, label in data:
+        emission_counts[label - 1][all_tokens.index(token)] += 1    #keep a count of how many times each token appears for each label in the data list
+    #last column filled with k value for UNK token
+    emission_counts[:, -1] = [k] * N
 
+    # calculate count(label)
     label_counts = []
     label_counts_dict = calculate_number_of_labels(data)    # a dictionary with the format : {label:count} , i.e, each key is a label which has its count as the corresponding value
     sorted_labels = sorted(label_counts_dict.keys())
     for label in sorted_labels:
         label_counts.append(label_counts_dict[label])
-    label_counts = np.array(label_counts)
+    label_counts = np.array(label_counts)                # in increasing order of label (from 0 to 8), contains (label:label count)
+    print(label_counts)
+
+    # calculate count(label->token)/count(label) for each label 
+    for index, value in enumerate(emission_counts): 
+        emission_parameters[index] = emission_counts[index] / (label_counts[index] + k)  #index - each row of 2d array
 
 
-    for token, label in data:
-        emission_counts[label - 1][all_unique_tokens.index(token)] += 1    #keep a count of how many times each token appears for each label in the data list
-    # This is for the other case of #UNK# tokens
-    emission_counts[:, -1] = [k] * N
+    return emission_parameters  #each value in this array gives the probability e(x|y) -> token given a label
 
 
-    for index, _ in enumerate(emission_counts):
-        emission_parameters[index] = emission_counts[index] / (label_counts[index] + k)
+# Get tag from word
+def get_label_from_token(token, emission_parameters, all_tokens, ):
+    if token in all_tokens:
+        column_to_consider = emission_parameters[:, all_tokens.index(token)]   
+    else:
+        column_to_consider = emission_parameters[:, -1]
+        
+    # Get the indices of maximum values in the array
+    max_indices = np.where(column_to_consider == column_to_consider.max())[0]
+    # Randomly choose an index from the maximum indices
+    x = random.choice(max_indices) + 1
+    return labels_list[x]
 
 
-    return emission_parameters
+def write_prediction_output_to_file(language):
+    if language == "EN":
+        # Conduct training/supervised learning (M-Step)
+        train_data = read_training_data(en_train_path)
+        all_tokens = get_all_tokens(train_data)
+        emission_parameters = calculate_emission_parameters(train_data, all_tokens)
+
+        # Execute testing/decoding (E-Step)
+        predicted_results = []
+        test_data = read_dev_in_data(en_dev_in_path)
+        for token in test_data:
+            if token:
+                predicted_results.append(token + " " + get_label_from_token(token, all_tokens, emission_parameters))
+            else:
+                predicted_results.append("")
+        with open(en_dev_p1_out_path, "w+", encoding="utf-8") as file:
+            for line in predicted_results:
+                file.write(line + "\n")
+
+    elif language == "FR":
+        # Conduct training/supervised learning (M-Step)
+        train_data = read_training_data(fr_train_path)
+        all_tokens = get_all_tokens(train_data)
+        emission_parameters = calculate_emission_parameters(train_data, all_tokens)
+
+        # Execute testing/decoding (E-Step)
+        predicted_results = []
+        test_data = read_dev_in_data(fr_dev_in_path)
+        for token in test_data:
+            if token:
+                predicted_results.append(token + " " + get_label_from_token(token, all_tokens, emission_parameters))
+            else:
+                predicted_results.append("")
+        with open(en_dev_p1_out_path, "w+", encoding="utf-8") as file:
+            for line in predicted_results:
+                file.write(line + "\n")
+for language in ["EN", "FR"]:
+    write_prediction_output_to_file(language)
